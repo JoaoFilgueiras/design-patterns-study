8e0e4e764ef2b78a0b06caf24a188c71d6d89f8b
JoaoFilgueiras <joaoantoniofilgueiras@gmail.com>
2021-09-24 11:21:41 -0300

    Config initial project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+    return (
+        <>
+            <h1>App config inicial</h1>
+        </>
+    );
+}
+
+export default App;
